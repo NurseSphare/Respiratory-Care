b059f1f42c420f1750028e30c68c11caeb601fc2
Nurse Sphare <hmssoon@gmail.com>
2026-05-24 22:13:35 +0400

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDisqDxl9qVCB_iJcEadrLYjvMr33YdT5I",
-  authDomain: "respiratory-team.firebaseapp.com",
-  databaseURL: "https://respiratory-team-default-rtdb.firebaseio.com",
-  projectId: "respiratory-team",
-  storageBucket: "respiratory-team.firebasestorage.app",
-  messagingSenderId: "731300314581",
-  appId: "1:731300314581:web:ec58be5de4659bc832d15c",
-  measurementId: "G-ZCTK58B2MK"
+/**
+ * respiratory-team — Firestore (البيانات المشتركة)
+ * Hosting منفصل على hd-ward — هذا طبيعي.
+ */
+window.FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyDisqDxl9qVCB_iJcEadrLYjvMr33YdT5I',
+  authDomain: 'respiratory-team.firebaseapp.com',
+  projectId: 'respiratory-team',
+  storageBucket: 'respiratory-team.firebasestorage.app',
+  messagingSenderId: '731300314581',
+  appId: '1:731300314581:web:ec58be5de4659bc832d15c'
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+window.FIREBASE_COLLECTION = 'challenges';
+window.FIREBASE_DOC_ID = 'respiratory-care-weekly';
